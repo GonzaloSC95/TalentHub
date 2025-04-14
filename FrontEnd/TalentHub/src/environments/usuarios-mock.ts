df110dbfd76abfe8dcfac5f854e12cdb3db00393
Ana González Bueno <anagbueno@gmail.com>
2025-04-14 08:37:28 +0200

    'roles'

--- FrontEnd/TalentHub/src/environments/usuarios-mock.ts
+++ FrontEnd/TalentHub/src/environments/usuarios-mock.ts
@@ -1,11 +1,11 @@
 import { Usuario } from "../app/interfaces/usuario";
 
 export const USUARIOS_DATA: Usuario[] = [
-    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'empresa',empresa: 'tocoto',},
-    {email: 'elsegundo@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'candidato',empresa: '',},
-    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'empresa',empresa: 'bichos',},
-    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'candidato',empresa: '',},
-    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'candidato',empresa: '',},
+    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'EMPRESA',empresa: 'tocoto',},
+    {email: 'elsegundo@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'CLIENTE',empresa: '',},
+    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'EMPRESA',empresa: 'bichos',},
+    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'CLIENTE',empresa: '',},
+    {email: 'elprimer@hotmail.com', password: 'alcalacrece', nombre: 'string', apellidos: 'string',enabled: 0, rol: 'CLIENTE',empresa: '',},
     {
         email: 'empresa@dominio.com',
         password: 'empresa123',
@@ -13,8 +13,8 @@ export const USUARIOS_DATA: Usuario[] = [
         apellidos: '',
         enabled: 1,
         fechaRegistro: new Date('2023-01-02'),
-        rol: 'empresa',
-        empresa: 'Empresa SA',
+        rol: 'EMPRESA',
+        empresa: '',
       },
       {
         email: 'admin@admin.com',
@@ -23,7 +23,7 @@ export const USUARIOS_DATA: Usuario[] = [
         apellidos: 'Admin',
         enabled: 1,
         fechaRegistro: new Date('2023-01-03'),
-        rol: 'admin',
+        rol: 'ADMON',
         empresa: '',
       },
 ]  
