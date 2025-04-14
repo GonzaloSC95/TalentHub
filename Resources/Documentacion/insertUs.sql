b09786626354856c2911ed863649e615766b90ea
Ana González Bueno <anagbueno@gmail.com>
2025-04-14 08:03:13 +0200

    insertUs

---
+++ Resources/Documentacion/insertUs.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `Usuarios` VALUES('elprimero@hotmail.com', 'Aarón', 'Rivero Gómez', 'alcalacrece',1,'2025-01-27' ,'CLIENTE');
+INSERT INTO `Usuarios` VALUES('elsegundo@hotmail.com', 'Adela', 'Salas Díaz', 'alcalacrece',1,'2025-01-27' ,'EMPRESA');
+INSERT INTO `Usuarios` VALUES('eltercero@hotmail.com', 'Adolfo', 'Rubio Flores', 'alcalacrece',1,'2025-01-27', 'ADMON');
+INSERT INTO `Usuarios` VALUES('elcuarto@hotmail.com', 'Adrián', 'Suárez', 'alcalacrece',1,'2025-01-28', 'CLIENTE');
+INSERT INTO `Usuarios` VALUES('elquinto@hotmail.com', 'Marcos', 'Loyola Méndez', 'alcalacrece',1, '2025-01-28','CLIENTE');
+INSERT INTO `Usuarios` VALUES('elsexto@hotmail.com', 'María', 'Santana Moreno', 'alcalacrece',1,'2025-01-28' ,'EMPRESA');
+INSERT INTO `Usuarios` VALUES('elseptimo@hotmail.com', 'Pilar', 'Ruiz', 'alcalacrece',1,'2025-01-28', 'CLIENTE');
+INSERT INTO `Usuarios` VALUES('eloctavo@hotmail.com', 'Pepe', 'Ruiz Santana', 'alcalacrece',1,'2025-01-27' , 'ADMON');
