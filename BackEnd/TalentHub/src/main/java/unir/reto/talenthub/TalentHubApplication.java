d85226996a3e984a4bf62ade6e594f3b4729f4c9
Ana González Bueno <anagbueno@gmail.com>
2025-05-09 19:12:58 +0200

    final despliegue

--- BackEnd/TalentHub/src/main/java/unir/reto/talenthub/TalentHubApplication.java
+++ BackEnd/TalentHub/src/main/java/unir/reto/talenthub/TalentHubApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TalentHubApplication extends SpringBootServletInitializer {
@@ -15,5 +18,18 @@ public class TalentHubApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TalentHubApplication.class);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                      .allowedOrigins("https://anagonbu.com")
+                      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                      .allowedHeaders("*")
+                      .allowCredentials(true);
+            }
+        };
     }
 }
